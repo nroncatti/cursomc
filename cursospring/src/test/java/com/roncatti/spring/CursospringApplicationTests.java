4058c042907efc182697012fcf61091297db006c
Nelson Roncatti <nroncatti@gmail.com>
2019-11-20 11:36:53 -0300

    first commit

---
+++ cursospring/src/test/java/com/roncatti/spring/CursospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roncatti.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
